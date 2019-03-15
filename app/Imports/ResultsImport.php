4bda009e84ebd75aadbf2b50f7db0a23d6fc1ec6
Carlita Wiggan <info@cwiggan.com>
2019-03-15 11:29:31 -0400

    update

--- app/Imports/ResultsImport.php
+++ app/Imports/ResultsImport.php
@@ -41,7 +41,7 @@ class ResultsImport implements ToModel, WithHeadingRow
             'distance' => $row['distance'],
             'laps' => $row['laps'],
             'age_group' => $row['ag'] ?? '',
-            'bid' => $row['bib'] ?? '',
+            'bid' => $row['bib'] ?? 0,
             'year' => $this->year,
             'gender' => $gender[$row['gender']],
             'time' => $row['time'] ?? '00:00:00'
