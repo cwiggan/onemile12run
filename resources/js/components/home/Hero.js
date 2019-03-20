453101e413002fa10b74f76f0aef873c7e6c6bf1
Carlita Wiggan <info@cwiggan.com>
2019-03-20 15:48:29 -0400

    update

--- resources/js/components/home/Hero.js
+++ resources/js/components/home/Hero.js
@@ -6,7 +6,7 @@ const Hero = () => (
         <div className="overlay">
             <div className="container">
                 <h1 className="text-center">
-                    <span className="top">One Mile With a Smile Run Series</span>
+                    <span className="top">One Mile With a Smile 12 Hour Run</span>
                     <span className="mid">August 10, 2019</span>
                     <span className="last">@Oak Grove Lake Park, Chesapeake, VA</span>
                 </h1>
